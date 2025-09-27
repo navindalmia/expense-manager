8f6bbee6ae9a0443e931fcea9e07eb498f49e555
navin <navindalmia2003@yahoo.co.in>
2025-09-27 16:27:05 +0100

    placeholder home screen

--- App.tsx
+++ App.tsx
@@ -19,12 +19,28 @@
 //   },
 // });
 
-import { Text, View } from 'react-native';
+// import { Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>lets start the fun! 👋</Text>
+//     </View>
+//   );
+// }
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>lets start the fun! 👋</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
